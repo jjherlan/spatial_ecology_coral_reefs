8109d7175e9b6201478deac2cd27991145add401
jjherlan <75087560+jjherlan@users.noreply.github.com>
2023-03-21 16:21:55 -0400

    commit and push, 1621, 3/21/2023

--- rpn_point_counts_2016.R
+++ rpn_point_counts_2016.R
@@ -137,14 +137,171 @@ summary(ana_sh.glm2_qu)
 # `Anova` function from the *car* package
 Anova(ana_sh.glm2_qu, type = "III") # Type III because... 
 
+# West Motu Tautara
 
+rpn_mtt_sh <- read.csv('mtt_sh_2016.csv', header = T) %>%
+  mutate_at(vars(transect, photo), factor) %>%
+  #group_by(transect) %>%
+  #filter(Tdir >= 90 & Tdir < 250) %>%
+  pivot_longer(cols = pt1:pt100,
+               names_to = "point",
+               values_to = "taxa",
+               values_drop_na = FALSE
+  ) %>%
+  group_by(transect, photo, taxa) %>%
+  dplyr::summarize(n = n()
+  ) %>%
+  #select(all_of(-c(X, X.1))) %>%
+  mutate(group = case_when(
+    taxa == "PLOB" ~ "plob",
+    taxa == "POCI" ~ "poci",
+    taxa == "PVER" ~ "poci",
+    taxa == "CALC" ~ "macro",
+    taxa == "CPOC" ~ "macro",
+    taxa == "DC" ~ "plob",
+    taxa == "DAUS" ~ "macro", 
+    taxa == "DCRE" ~ "macro",
+    taxa == "EAM" ~ "turf",
+    taxa == "HALI" ~ "macro",
+    taxa == "LVAR" ~ "macro",
+    taxa == "MACRO_UNK" ~ "macro",
+    taxa == "O" ~ "biotic",
+    taxa == "PLIG" ~ "poci",
+    taxa == "SHAD" ~ "non-reef",
+    taxa == "SOBT" ~ "macro",
+    taxa == "SPR" ~ "abiotic",
+    taxa == "TAPE" ~ "non-reef",
+    taxa == "TURF" ~ "turf",
+    taxa == "UNK" ~ "non-reef",
+    taxa == "WAND" ~ "non-reef"
+  )
+  ) %>%
+  group_by(photo, group) %>%
+  mutate_at(vars(group), factor) %>%
+  dplyr::summarize(successes = sum(n)
+  ) %>%
+  mutate(
+    failures = 100 - successes)
 
+rpn_mtt_sh %>%
+  complete(group)
 
+rpn_mtt_sh2 <-
+rpn_mtt_sh %>%
+  complete(group)
 
+write.csv(rpn_mtt_sh, 'rpn_mtt_sh.csv')
+write.csv(rpn_mtt_sh2, 'rpn_mtt_sh2.csv')
 
+# West Motu Tautara Mid Depth
 
+rpn_mtt_md <- read.csv('mtt_md_2016.csv', header = T) %>%
+  mutate_at(vars(transect, photo), factor) %>%
+  #group_by(transect) %>%
+  #filter(Tdir >= 90 & Tdir < 250) %>%
+  pivot_longer(cols = pt1:pt100,
+               names_to = "point",
+               values_to = "taxa",
+               values_drop_na = FALSE
+  ) %>%
+  group_by(transect, photo, taxa) %>%
+  dplyr::summarize(n = n()
+  ) %>%
+  #select(all_of(-c(X, X.1))) %>%
+  mutate(group = case_when(
+    taxa == "PLOB" ~ "plob",
+    taxa == "POCI" ~ "poci",
+    taxa == "PVER" ~ "poci",
+    taxa == "CALC" ~ "macro",
+    taxa == "CPOC" ~ "macro",
+    taxa == "DC" ~ "plob",
+    taxa == "DAUS" ~ "macro", 
+    taxa == "DCRE" ~ "macro",
+    taxa == "EAM" ~ "turf",
+    taxa == "HALI" ~ "macro",
+    taxa == "LVAR" ~ "macro",
+    taxa == "MACRO_UNK" ~ "macro",
+    taxa == "O" ~ "biotic",
+    taxa == "PLIG" ~ "poci",
+    taxa == "SHAD" ~ "non-reef",
+    taxa == "SOBT" ~ "macro",
+    taxa == "SPR" ~ "abiotic",
+    taxa == "TAPE" ~ "non-reef",
+    taxa == "TURF" ~ "turf",
+    taxa == "UNK" ~ "non-reef",
+    taxa == "WAND" ~ "non-reef"
+  )
+  ) %>%
+  group_by(photo, group) %>%
+  mutate_at(vars(group), factor) %>%
+  dplyr::summarize(successes = sum(n)
+  ) %>%
+  mutate(
+    failures = 100 - successes)
 
+rpn_mtt_md %>%
+  complete(group)
 
+rpn_mtt_md <-
+  rpn_mtt_md %>%
+  complete(group)
+
+write.csv(rpn_mtt_md, 'rpn_mtt_md.csv')
+
+# West Motu Tautara Deep Depth
+
+rpn_mtt_dp <- read.csv('mtt_dp_2016.csv', header = T) %>%
+  mutate_at(vars(transect, photo), factor) %>%
+  #group_by(transect) %>%
+  #filter(Tdir >= 90 & Tdir < 250) %>%
+  pivot_longer(cols = pt1:pt100,
+               names_to = "point",
+               values_to = "taxa",
+               values_drop_na = FALSE
+  ) %>%
+  group_by(transect, photo, taxa) %>%
+  dplyr::summarize(n = n()
+  ) %>%
+  #select(all_of(-c(X, X.1))) %>%
+  mutate(group = case_when(
+    taxa == "PLOB" ~ "plob",
+    taxa == "POCI" ~ "poci",
+    taxa == "PVER" ~ "poci",
+    taxa == "CALC" ~ "macro",
+    taxa == "CPOC" ~ "macro",
+    taxa == "DC" ~ "plob",
+    taxa == "DAUS" ~ "macro", 
+    taxa == "DCRE" ~ "macro",
+    taxa == "EAM" ~ "turf",
+    taxa == "HALI" ~ "macro",
+    taxa == "LVAR" ~ "macro",
+    taxa == "MACRO_UNK" ~ "macro",
+    taxa == "O" ~ "biotic",
+    taxa == "PLIG" ~ "poci",
+    taxa == "SHAD" ~ "non-reef",
+    taxa == "SOBT" ~ "macro",
+    taxa == "SPR" ~ "abiotic",
+    taxa == "TAPE" ~ "non-reef",
+    taxa == "TURF" ~ "turf",
+    taxa == "UNK" ~ "non-reef",
+    taxa == "WAND" ~ "non-reef"
+  )
+  ) %>%
+  group_by(photo, group) %>%
+  mutate_at(vars(group), factor) %>%
+  dplyr::summarize(successes = sum(n)
+  ) %>%
+  mutate(
+    failures = 100 - successes)
+
+rpn_mtt_dp %>%
+  complete(group)
+
+rpn_mtt_dp <-
+  rpn_mtt_dp %>%
+  complete(group)
+
+write.csv(rpn_mtt_dp, 'rpn_mtt_dp.csv')
 
 
 
