c1952c0e855e5f59c0c51007cb234737bc48c3a6
jjherlan <75087560+jjherlan@users.noreply.github.com>
2023-04-22 12:36:58 -0400

    commit and push, 1236, 4/22/2023

--- bleaching.R
+++ bleaching.R
@@ -1,3 +1,9 @@
+require(tidyverse)
+require(cowplot)
+require(ggpubr)
+theme_set(theme_cowplot())
+
+# Porites lobata
 
 rpn_plob_2015.ggbarplot <- ggplot(rpn_plob_summ.gg, aes(x = factor(coast, x_labels), y = mean, fill = coast)) +   
   geom_bar(stat = "identity", width = 0.75, color = "black", linewidth = 0.50, alpha = 0.6) +
@@ -7,7 +13,43 @@ rpn_plob_2015.ggbarplot <- ggplot(rpn_plob_summ.gg, aes(x = factor(coast, x_labe
   scale_x_discrete(expand = c(0, 1), labels = x_labels) + 
   scale_fill_manual(values = c("#FFC74E", "#82A5C0", "#ABC178")) + #
   facet_wrap( ~ depth2, labeller = as_labeller(label_names), dir = "v", ncol = 1) + 
-  ggtitle(expression(paste(italic(" Porites "), "sp."))) +
+  ggtitle(expression(paste(italic(" Porites "), "spp."))) +
+  #geom_text(aes(label = cld, y = upper.ci), vjust = -0.5, size = 10) +
+  #scale_y_log10(expression(paste("Colony Size (", cm^2, ")"), limits = c(0, 100000))) +
+  labs(x = NULL) +
+  theme(strip.text = element_text(size = 10, color = "black", hjust = 0.50),
+        strip.background = element_rect(fill = "#FFFFFF", color = NA),    
+        panel.background = element_rect(fill = "#FFFFFF", color = NA),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.y = element_line(color = "#b2b2b2"),
+        panel.spacing.x = unit(1, "cm"),
+        panel.spacing.y = unit(0.5, "cm"),
+        panel.spacing = unit(1, "lines"),
+        axis.ticks = element_blank(),
+        legend.position = 'right',
+        plot.title = element_text(size = 11),
+        axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 12),
+        axis.title.y = element_text(size = 14),
+        legend.title = element_blank())
+
+rpn_plob_2015.ggbarplot
+
+#rpn_plob_summ.gg$coast = c('North', 'West', 'Southeast')
+leg1 <- get_legend(rpn_plob_2015.ggbarplot)
+leg1 <- as_ggplot(rpn_plob_2015.ggbarplot)
+
+rpn_poci_2015.ggbarplot <- ggplot(rpn_poci_summ.gg, aes(x = factor(coast, x_labels), y = mean, fill = coast)) +   
+  geom_bar(stat = "identity", width = 0.75, color = "black", linewidth = 0.50, alpha = 0.6) +
+  geom_linerange(aes(ymin = mean, ymax = mean + sd), linewidth = 0.75) +
+  scale_y_continuous(expression(paste("Percent Cover")), limits = c(0, 1.0), 
+                     labels = function(x) paste0(x*100)) + 
+  scale_x_discrete(expand = c(0, 1), labels = x_labels) + 
+  scale_fill_manual(values = c("#FFC74E", "#82A5C0", "#ABC178")) + #
+  facet_wrap( ~ depth2, labeller = as_labeller(label_names), dir = "v", ncol = 1) + 
+  ggtitle(expression(paste(italic(" Pocillopora "), "spp."))) +
   #geom_text(aes(label = cld, y = upper.ci), vjust = -0.5, size = 10) +
   #scale_y_log10(expression(paste("Colony Size (", cm^2, ")"), limits = c(0, 100000))) +
   labs(x = NULL) +
@@ -23,10 +65,312 @@ rpn_plob_2015.ggbarplot <- ggplot(rpn_plob_summ.gg, aes(x = factor(coast, x_labe
         panel.spacing = unit(1, "lines"),
         axis.ticks = element_blank(),
         legend.position = 'none',
-        #plot.title = element_text(size = 11),
+        plot.title = element_text(size = 11),
         axis.text.y = element_text(size = 10),
         axis.text.x = element_text(size = 12),
         axis.title.y = element_text(size = 14),
         legend.title = element_blank())
 
-rpn_plob_2015.ggbarplot
+rpn_poci_2015.ggbarplot
+
+
+# Macrolagae
+
+rpn_ma_2015.ggbarplot <- ggplot(rpn_ma_summ.gg, aes(x = factor(coast, x_labels), y = mean, fill = coast)) +   
+  geom_bar(stat = "identity", width = 0.75, color = "black", linewidth = 0.50, alpha = 0.6) +
+  geom_linerange(aes(ymin = mean, ymax = mean + sd), linewidth = 0.75) +
+  scale_y_continuous(expression(paste("Percent Cover")), limits = c(0, 1.0), 
+                     labels = function(x) paste0(x*100)) + 
+  scale_x_discrete(expand = c(0, 1), labels = x_labels) + 
+  scale_fill_manual(values = c("#FFC74E", "#82A5C0", "#ABC178")) + #
+  facet_wrap( ~ depth2, labeller = as_labeller(label_names), dir = "v", ncol = 1) + 
+  ggtitle(expression(paste(italic(" Macrolagae ")))) +
+  #geom_text(aes(label = cld, y = upper.ci), vjust = -0.5, size = 10) +
+  #scale_y_log10(expression(paste("Colony Size (", cm^2, ")"), limits = c(0, 100000))) +
+  labs(x = NULL) +
+  theme(strip.text = element_text(size = 10, color = "black", hjust = 0.50),
+        strip.background = element_rect(fill = "#FFFFFF", color = NA),    
+        panel.background = element_rect(fill = "#FFFFFF", color = NA),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.y = element_line(color = "#b2b2b2"),
+        panel.spacing.x = unit(1, "cm"),
+        panel.spacing.y = unit(0.5, "cm"),
+        panel.spacing = unit(1, "lines"),
+        axis.ticks = element_blank(),
+        legend.position = 'none',
+        plot.title = element_text(size = 11),
+        axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 12),
+        axis.title.y = element_text(size = 14),
+        legend.title = element_blank())
+
+rpn_ma_2015.ggbarplot
+
+# Turf
+
+rpn_turf_2015.ggbarplot <- ggplot(rpn_turf_summ.gg, aes(x = factor(coast, x_labels), y = mean, fill = coast)) +   
+  geom_bar(stat = "identity", width = 0.75, color = "black", linewidth = 0.50, alpha = 0.6) +
+  geom_linerange(aes(ymin = mean, ymax = mean + sd), linewidth = 0.75) +
+  scale_y_continuous(expression(paste("Percent Cover")), limits = c(0, 1.0), 
+                     labels = function(x) paste0(x*100)) + 
+  scale_x_discrete(expand = c(0, 1), labels = x_labels) + 
+  scale_fill_manual(values = c("#FFC74E", "#82A5C0", "#ABC178")) + #
+  facet_wrap( ~ depth2, labeller = as_labeller(label_names), dir = "v", ncol = 1) + 
+  ggtitle(expression(paste(italic(" Turf ")))) +
+  #geom_text(aes(label = cld, y = upper.ci), vjust = -0.5, size = 10) +
+  #scale_y_log10(expression(paste("Colony Size (", cm^2, ")"), limits = c(0, 100000))) +
+  labs(x = NULL) +
+  theme(strip.text = element_text(size = 10, color = "black", hjust = 0.50),
+        strip.background = element_rect(fill = "#FFFFFF", color = NA),    
+        panel.background = element_rect(fill = "#FFFFFF", color = NA),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.y = element_line(color = "#b2b2b2"),
+        panel.spacing.x = unit(1, "cm"),
+        panel.spacing.y = unit(0.5, "cm"),
+        panel.spacing = unit(1, "lines"),
+        axis.ticks = element_blank(),
+        legend.position = 'none',
+        plot.title = element_text(size = 11),
+        axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 12),
+        axis.title.y = element_text(size = 14),
+        legend.title = element_blank())
+
+rpn_turf_2015.ggbarplot
+
+# Sand
+
+rpn_sand_2015.ggbarplot <- ggplot(rpn_sand_summ.gg, aes(x = factor(coast, x_labels), y = mean, fill = coast)) +   
+  geom_bar(stat = "identity", width = 0.75, color = "black", linewidth = 0.50, alpha = 0.6) +
+  geom_linerange(aes(ymin = mean, ymax = mean + sd), linewidth = 0.75) +
+  scale_y_continuous(expression(paste("Percent Cover")), limits = c(0, 1.0), 
+                     labels = function(x) paste0(x*100)) + 
+  scale_x_discrete(expand = c(0, 1), labels = x_labels) + 
+  scale_fill_manual(values = c("#FFC74E", "#82A5C0", "#ABC178")) + #
+  facet_wrap( ~ depth2, labeller = as_labeller(label_names), dir = "v", ncol = 1) + 
+  ggtitle(expression(paste(italic(" Sand ")))) +
+  #geom_text(aes(label = cld, y = upper.ci), vjust = -0.5, size = 10) +
+  #scale_y_log10(expression(paste("Colony Size (", cm^2, ")"), limits = c(0, 100000))) +
+  labs(x = NULL) +
+  theme(strip.text = element_text(size = 10, color = "black", hjust = 0.50),
+        strip.background = element_rect(fill = "#FFFFFF", color = NA),    
+        panel.background = element_rect(fill = "#FFFFFF", color = NA),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.y = element_line(color = "#b2b2b2"),
+        panel.spacing.x = unit(1, "cm"),
+        panel.spacing.y = unit(0.5, "cm"),
+        panel.spacing = unit(1, "lines"),
+        axis.ticks = element_blank(),
+        legend.position = 'none',
+        plot.title = element_text(size = 11),
+        axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 12),
+        axis.title.y = element_text(size = 14),
+        legend.title = element_blank())
+
+rpn_sand_2015.ggbarplot
+
+# Bare substrate = calcium carbonate (dead coral) + basalt (lava)
+
+rpn_bare_2015.ggbarplot <- ggplot(rpn_bare_summ.gg, aes(x = factor(coast, x_labels), y = mean, fill = coast)) +   
+  geom_bar(stat = "identity", width = 0.75, color = "black", linewidth = 0.50, alpha = 0.6) +
+  geom_linerange(aes(ymin = mean, ymax = mean + sd), linewidth = 0.75) +
+  scale_y_continuous(expression(paste("Percent Cover")), limits = c(0, 1.0), 
+                     labels = function(x) paste0(x*100)) + 
+  scale_x_discrete(expand = c(0, 1), labels = x_labels) + 
+  scale_fill_manual(values = c("#FFC74E", "#82A5C0", "#ABC178")) + #
+  facet_wrap( ~ depth2, labeller = as_labeller(label_names), dir = "v", ncol = 1) + 
+  ggtitle(expression(paste(italic(" Bare ")))) +
+  #geom_text(aes(label = cld, y = upper.ci), vjust = -0.5, size = 10) +
+  #scale_y_log10(expression(paste("Colony Size (", cm^2, ")"), limits = c(0, 100000))) +
+  labs(x = NULL) +
+  theme(strip.text = element_text(size = 10, color = "black", hjust = 0.50),
+        strip.background = element_rect(fill = "#FFFFFF", color = NA),    
+        panel.background = element_rect(fill = "#FFFFFF", color = NA),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.y = element_line(color = "#b2b2b2"),
+        panel.spacing.x = unit(1, "cm"),
+        panel.spacing.y = unit(0.5, "cm"),
+        panel.spacing = unit(1, "lines"),
+        axis.ticks = element_blank(),
+        legend.position = 'none',
+        plot.title = element_text(size = 11),
+        axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 12),
+        axis.title.y = element_text(size = 14),
+        legend.title = element_blank())
+
+rpn_bare_2015.ggbarplot
+
+# CCA
+
+rpn_cca_2015.ggbarplot <- ggplot(rpn_cca_summ.gg, aes(x = factor(coast, x_labels), y = mean, fill = coast)) +   
+  geom_bar(stat = "identity", width = 0.75, color = "black", linewidth = 0.50, alpha = 0.6) +
+  geom_linerange(aes(ymin = mean, ymax = mean + sd), linewidth = 0.75) +
+  scale_y_continuous(expression(paste("Percent Cover")), limits = c(0, 1.0), 
+                     labels = function(x) paste0(x*100)) + 
+  scale_x_discrete(expand = c(0, 1), labels = x_labels) + 
+  scale_fill_manual(values = c("#FFC74E", "#82A5C0", "#ABC178")) + #
+  facet_wrap( ~ depth2, labeller = as_labeller(label_names), dir = "v", ncol = 1) + 
+  ggtitle(expression(paste(italic(" CCA ")))) +
+  #geom_text(aes(label = cld, y = upper.ci), vjust = -0.5, size = 10) +
+  #scale_y_log10(expression(paste("Colony Size (", cm^2, ")"), limits = c(0, 100000))) +
+  labs(x = NULL) +
+  theme(strip.text = element_text(size = 10, color = "black", hjust = 0.50),
+        strip.background = element_rect(fill = "#FFFFFF", color = NA),    
+        panel.background = element_rect(fill = "#FFFFFF", color = NA),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.y = element_line(color = "#b2b2b2"),
+        panel.spacing.x = unit(1, "cm"),
+        panel.spacing.y = unit(0.5, "cm"),
+        panel.spacing = unit(1, "lines"),
+        axis.ticks = element_blank(),
+        legend.position = 'none',
+        plot.title = element_text(size = 11),
+        axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 12),
+        axis.title.y = element_text(size = 14),
+        legend.title = element_blank())
+
+rpn_cca_2015.ggbarplot
+
+rpn_plob_2015 <-
+rpn_plob_summ.gg %>%
+  add_column(group = rep(c('plob'), times = 6)) %>%
+  mutate_at(vars(group), factor)
+
+rpn_poci_2015 <-
+  rpn_poci_summ.gg %>%
+  add_column(group = rep(c('poci'), times = 6)) %>%
+  mutate_at(vars(group), factor)
+
+rpn_ma_2015 <-
+  rpn_ma_summ.gg %>%
+  add_column(group = rep(c('ma'), times = 6)) %>%
+  mutate_at(vars(group), factor)
+
+rpn_turf_2015 <-
+  rpn_turf_summ.gg %>%
+  add_column(group = rep(c('turf'), times = 6)) %>%
+  mutate_at(vars(group), factor)
+
+rpn_cca_2015 <-
+  rpn_cca_summ.gg %>%
+  add_column(group = rep(c('cca'), times = 6)) %>%
+  mutate_at(vars(group), factor)
+
+rpn_bare_2015 <-
+  rpn_bare_summ.gg %>%
+  add_column(group = rep(c('bare'), times = 6)) %>%
+  mutate_at(vars(group), factor)
+
+rpn_sand_2015 <-
+  rpn_sand_summ.gg %>%
+  add_column(group = rep(c('sand'), times = 6)) %>%
+  mutate_at(vars(group), factor)
+
+rpn_cover_groups <-
+  bind_rows(
+  rpn_plob_2015,
+  rpn_poci_2015,
+  rpn_ma_2015,
+  rpn_turf_2015,
+  rpn_cca_2015,
+  rpn_bare_2015,
+  rpn_sand_2015)
+
+# par(mfrow = c(3, 4))
+
+plot_grid(
+
+rpn_plob_2015.ggbarplot,
+rpn_poci_2015.ggbarplot,
+rpn_ma_2015.ggbarplot,
+rpn_turf_2015.ggbarplot,
+rpn_cca_2015.ggbarplot,
+rpn_bare_2015.ggbarplot,
+rpn_sand_2015.ggbarplot
+
+#labels = c('A', 'B')
+
+)
+
+group_labels = c("plob" = "PLOB", 
+             "poci" = "POCI",
+             "ma" = "MA",
+             "turf" = "Turf",
+             "cca" = "CCA",
+             "bare" = "Bare",
+             "sand" = "Sand")
+
+rpn_2015.ggbarplot <- ggplot(rpn_cover_groups, aes(x = factor(coast, x_labels), y = mean, fill = coast)) +   
+  geom_bar(stat = "identity", width = 0.75, color = "black", linewidth = 0.50, alpha = 0.6) +
+  geom_linerange(aes(ymin = mean, ymax = mean + sd), linewidth = 0.75) +
+  scale_y_continuous(expression(paste("Percent Cover")), limits = c(0, 1.0), 
+                     labels = function(x) paste0(x*100)) + 
+  scale_x_discrete(expand = c(0, 1), labels = x_labels) + 
+  scale_fill_manual(values = c("#FFC74E", "#82A5C0", "#ABC178")) + #
+  facet_grid(group ~ depth2, margin = FALSE, labeller = labeller(group = group_labels)) + 
+  #ggtitle(expression(paste(italic(" Porites "), "spp."))) +
+  #geom_text(aes(label = cld, y = upper.ci), vjust = -0.5, size = 10) +
+  #scale_y_log10(expression(paste("Colony Size (", cm^2, ")"), limits = c(0, 100000))) +
+  labs(x = NULL) +
+  theme(strip.text = element_text(size = 10, color = "black", hjust = 0.50),
+        strip.background = element_rect(fill = "#FFFFFF", color = NA),    
+        panel.background = element_rect(fill = "#FFFFFF", color = NA),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.y = element_line(color = "#b2b2b2"),
+        panel.spacing.x = unit(1, "cm"),
+        panel.spacing.y = unit(0.5, "cm"),
+        panel.spacing = unit(1, "lines"),
+        axis.ticks = element_blank(),
+        legend.position = 'right',
+        plot.title = element_text(size = 11),
+        axis.text.y = element_text(size = 10),
+        axis.text.x = element_blank(),
+        axis.title.y = element_text(size = 14),
+        legend.title = element_blank())
+
+rpn_2015.ggbarplot
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
